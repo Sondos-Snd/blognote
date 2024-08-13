a0264536f41ae9d399dade9af30edc37efb30c38
Sondos-Snd <awachri.sondos@gmail.com>
2024-08-13 14:28:12 +0100

    entities + repositories

--- api/blognote/blognote/src/main/java/com/opensource/blognote/BlognoteApplication.java
+++ api/blognote/blognote/src/main/java/com/opensource/blognote/BlognoteApplication.java
@@ -2,8 +2,10 @@ package com.opensource.blognote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BlognoteApplication {
 
 	public static void main(String[] args) {
