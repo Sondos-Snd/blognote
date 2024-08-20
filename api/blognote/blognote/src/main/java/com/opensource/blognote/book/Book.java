adf7bfbe142018fdeae743cbfa11e45b5332ebbe
Sondos-Snd <awachri.sondos@gmail.com>
2024-08-20 18:49:46 +0100

    base entity

--- api/blognote/blognote/src/main/java/com/opensource/blognote/book/Book.java
+++ api/blognote/blognote/src/main/java/com/opensource/blognote/book/Book.java
@@ -1,20 +1,20 @@
 package com.opensource.blognote.book;
 
-import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Book {
-
-    @Id
-    @GeneratedValue
-    private Integer id;
+public class Book extends BaseEntity {
 
     private String title;
 
@@ -29,16 +29,5 @@ public class Book {
     private boolean archived;
 
     private boolean shareable;
-    @CreatedDate
-    @Column(nullable = false,updatable = false)
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-    @CreatedBy
-    @Column(nullable = false,updatable = false)
-    private Integer createdBy;
-    @LastModifiedDate
-    @Column(nullable = false)
-    private Integer lastModifiedBy;
+
 }
--- api/blognote/blognote/src/main/java/com/opensource/blognote/feedback/Feedback.java
+++ api/blognote/blognote/src/main/java/com/opensource/blognote/feedback/Feedback.java
@@ -1,35 +1,24 @@
 package com.opensource.blognote.feedback;
 
-import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Feedback {
-    @Id
-    @GeneratedValue
-    private Integer id;
+public class Feedback extends BaseEntity {
 
     private Double note;
 
     private String comment;
 
-    @CreatedDate
-    @Column(nullable = false,updatable = false)
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-    @CreatedBy
-    @Column(nullable = false,updatable = false)
-    private Integer createdBy;
-    @LastModifiedDate
-    @Column(nullable = false)
-    private Integer lastModifiedBy;
 
 }
