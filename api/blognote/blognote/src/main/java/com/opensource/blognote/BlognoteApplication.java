d45fc08ed7080b543f48f06dca5cc1f94f3a1a7c
Sondos-Snd <awachri.sondos@gmail.com>
2024-08-20 11:32:15 +0100

    mail dev sending + user role fix

--- api/blognote/blognote/src/main/java/com/opensource/blognote/BlognoteApplication.java
+++ api/blognote/blognote/src/main/java/com/opensource/blognote/BlognoteApplication.java
@@ -1,7 +1,11 @@
 package com.opensource.blognote;
 
+import com.opensource.blognote.role.Role;
+import com.opensource.blognote.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,6 +16,18 @@ public class BlognoteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlognoteApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner runner (RoleRepository roleRrpository){
+		return args -> {
+			if (roleRrpository.findByName("USER").isEmpty()) {
+				roleRrpository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
 	}
 
 }
