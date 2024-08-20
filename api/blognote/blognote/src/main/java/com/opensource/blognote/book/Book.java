547b08c5e4d4817314d70f92f3c6610b1c80362e
Sondos-Snd <awachri.sondos@gmail.com>
2024-08-20 18:41:33 +0100

    book entity

---
+++ api/blognote/blognote/src/main/java/com/opensource/blognote/book/Book.java
@@ -0,0 +1,45 @@
+package com.opensource.blognote.book;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String title;
+
+    private String authorName;
+
+    private String isbn;
+
+    private String synopsis;
+
+    private String bookCover;
+
+    private boolean archived;
+
+    private boolean shareable;
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+    @CreatedBy
+    @Column(nullable = false,updatable = false)
+    private Integer createdBy;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Integer lastModifiedBy;
+}
