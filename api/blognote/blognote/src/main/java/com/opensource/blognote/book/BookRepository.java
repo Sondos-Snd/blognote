53f48f8165cd13633cc5b192b43c82917a70617c
Sondos-Snd <awachri.sondos@gmail.com>
2024-08-21 11:58:13 +0100

    find by owner + specifications

--- api/blognote/blognote/src/main/java/com/opensource/blognote/book/BookRepository.java
+++ api/blognote/blognote/src/main/java/com/opensource/blognote/book/BookRepository.java
@@ -2,10 +2,12 @@ package com.opensource.blognote.book;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public interface BookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book> {
 
     @Query("""
             SELECT book
@@ -15,4 +17,5 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
             AND book.owner.id != :userId        
             """)
     Page<Book> findAllDisplayableBooks(Pageable pageable, Integer userId);
+
 }
